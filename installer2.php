6da36be5133edf06a9ab548c0e70b5cf5eed9b4f
Shailendra Singh Tiwari <singhtiwari.shailendra@gmail.com>
2014-04-11 18:34:23 +0530

    Create installer2.php

---
+++ installer2.php
@@ -0,0 +1,39 @@
+<html>
+<head>
+	<title> GSTI | Installer</title>
+		
+	<style type="text/css">
+		body{font-family:Arial,'Helvetica Neue',Helvetica,sans-serif;text-align:center;background:#3c5a76 ;}
+		h1{color:#fff;font-size:45px;font-weight:bold;text-shadow:5px 5px 8px rgba(0,0,0,0.55);letter-spacing:-1px;margin:0}
+		#msg{background:rgba(38, 38, 38, .30);width:800px;height:480px; margin:80px 0 0 280px;box-shadow:0 0 35px 3px #fff ;border-radius:10px 10px;}
+		#msg3{padding-top:20px; }
+		h2{text-shadow:0 1px 1px rgba(185,185,185,0.55);color:#243748;font-size:33px;line-height:35px;}
+		#msg2{border-radius:45px 45px;width:600px;padding:2px 20px 0 20px;  margin:20px 0 0 80px;border:solid 2px #ccc;}
+		#btn ul{display:block; margin:40px 50px 0 0 ;}
+		#btn li{float:right;list-style:none; width:155px;margin-right:125px;padding-top:-2px;}
+		#btn li a:link {box-shadow:0 0 32px 0 #fff inset ;background:rgb(40, 48, 59);border-radius:20px 20px; font-size:22px;line-height:45px; padding:0 ; display: block;font-weight:normal; color: #ffffff; text-align:center; text-decoration:none; text-transform: uppercase;}
+		#btn li a:visited {box-shadow:0 0 32px 0 #fff inset ;background:rgb(40, 48, 59);border-radius:20px 20px;  margin-right:2px; height:35px;padding:0 ; display: block;font-weight:normal; color: #ffffff; text-align:center; text-decoration:none; text-transform: uppercase;}
+		#btn li a:hover {box-shadow:0 0 32px 0 #fff inset ; background:black;border-radius:20px 20px;   line-height:45px;padding:0 ; display: block;font-weight:normal; color:#fff; text-align:center; text-decoration:none; text-transform: uppercase; }
+		#chk{line-height:45px;}
+	</style>
+</head>
+<body>
+	<div id="msg">
+	<div id="msg3">
+	<h1>Welcome to the  <i>GSTI Construction System</i>  Setup Wizard</h1>
+	<div id="msg2">
+     <form method="get" action="createdb.php">
+		<h2>Create Data Base <u>csys</u> on the MySql Server</h2>
+		 
+	  
+	</div>
+	<div id="btn">
+		<ul>
+		<li><a href="1.html#slogan2">Cancel</a></li> 
+			<li><a href="http://localhost/phpmyadmin/">Next </a></li> 
+			 
+		</ul>
+	</div>
+	</div></div>
+</body>
+</html>
