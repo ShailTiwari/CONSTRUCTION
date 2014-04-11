cface4ec5f43125fddd3628703a53e4ba99c3273
Shailendra Singh Tiwari <singhtiwari.shailendra@gmail.com>
2014-04-11 18:30:41 +0530

    Create about_us.php

---
+++ about_us.php
@@ -0,0 +1,190 @@
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+		<title>devloper</title>
+		<link rel="icon" type="image/gif" href="R.gif" >
+		<link href="1.css" rel="stylesheet">
+		<link rel="stylesheet" href="CSS/style.css" type="text/css">
+		<script type="text/javascript" src="js/registerv.js"></script>
+		<script type="text/javascript" src="js/jquery.min.js"></script>
+		<script type="text/javascript" src="js/jquery.js"></script>
+		<script type="text/javascript" src="js/menubar.js"></script>
+        <style type="text/css">
+	
+	
+* { margin:0;
+	padding:0;
+}
+
+html {height: 100%;}
+body{position: relative;
+	height: 100%;}
+.backgrounds{height: 100%;	
+	background: url(img/2.jpg);
+	background: -webkit-gradient(linear, left top, left bottom, from(#ccc), to(#fff));
+
+}
+
+.navbox {
+	position: relative;
+	float: left;
+}
+
+ul.nav {
+	list-style: none;
+	display: block;
+	width: 200px;
+	position: relative;
+	top: 50px;
+	left: 50px;
+	padding: 60px 0 60px 0;
+	background: url(css/image/shad2.png) no-repeat;
+	-webkit-background-size: 50% 100%;
+}
+
+li {
+	margin: 5px 0 0 0;
+}
+
+ul.nav li a {
+	-webkit-transition: all 0.3s ease-out;
+	background: #cbcbcb url(css/image/border.png) no-repeat;
+	color: #174867;
+	padding: 7px 15px 7px 15px;
+	-webkit-border-top-right-radius: 10px;
+ 	-webkit-border-bottom-right-radius: 10px;
+	width: 100px;
+	display: block;
+	text-decoration: none;
+	-webkit-box-shadow: 2px 2px 4px #888;
+}
+
+ul.nav li a:hover {
+	background: #ebebeb url(css/image/border.png) no-repeat;
+	color: #67a5cd;
+	padding: 7px 15px 7px 30px;
+}
+
+</style>
+		<script type="text/javascript" src="js/jquery.form.js"></script>
+<script type="text/javascript"> 
+$(document).ready(function(){
+
+$("#raj").hide();
+$("#raj2").hide();
+$("#raj5").hide();
+	$('#rd').click(function () {
+		$("#raj2").hide();
+		$("#raj5").hide();
+            $('#raj').toggle(500);
+            });
+	$('#rd2').click(function () {
+		$("#raj").hide();
+		$("#raj5").hide();			
+			$('#raj2').toggle(500);
+					});
+	
+		$('#rd5').click(function () {
+		$("#raj2").hide();
+		$("#raj").hide();				
+			$('#raj5').toggle(500);
+					});			
+				});
+</script>
+
+
+		
+		
+	</head>
+	<body>
+	
+
+<header>
+  <nav>
+    <a href="1.html#slogan1">Home</a>
+    <a href="1.html#slogan2">About</a>
+    <a href="1.html#slogan3">Design</a>
+    <a href="1.html#slogan4">Working</a>
+  </nav>
+</header>
+		<div class="Wrapper" style="padding-top: 120px;">
+		
+					<div class="navbox">
+				<ul class="nav">
+					<li><a id="rd" href="#">Shailendra</a></li>
+					<li><a id="rd2" href="#">Sonu</a></li>
+					<li><a id="rd5" href="#">Help & Guidence</a></li>
+				</ul>
+			</div>
+			<div style="float:right;background:rgba(200, 200, 200, .60);border-radius:5px 5px;padding:5px;margin:12px; height:auto;">
+			<div id="raj">
+				 
+				<h1 style="box-shadow:0px 10px 50px 10px rgba(15, 94, 114, 0.56) inset;color:rgba(48, 223, 238, 0.92);text-align:center;">SHAILENDRA SINGH TIWARI</h1>
+								 <img src="img/s.jpg" width="150px" height="200px" style="float:right;margin:2px;">
+
+				<div style="padding-top:10px; font-size:18px;width:500PX;height:300PX;">
+				<p>The All Hardwork of his
+				team into the Programming He simply Transformed 
+				</p>
+				<br/>
+				
+				<hr/>
+				<p><B>Role:</b> Programmer , Designer</p>
+				<hr/>
+				<p><B>Skill:</b>  PHP(basic), HTML(5), CSS(3),SQL</p>
+				<hr/>
+				<p style="font-size:22px;"><span style="font-size:42px;">SGSITS Indore</span> <br/> 
+				 
+				<span style="font-size:22px;">"Computer Engineering</span></p>
+				</div>
+			</div>
+			<div id="raj2">
+		
+			 
+				<h1 style="box-shadow:0px 10px 50px 10px rgba(15, 94, 114, 0.56) inset;color:rgba(196, 45, 255, 0.97);text-align:center;">SONU VERMA</h1>
+								 <img src="img/ss.jpg" width="150px" height="200px" style="float:right;margin:2px;">
+
+				<div style="padding-top:10px; font-size:18px;width:500PX;height:300PX;"">
+				<p> 
+				</p>
+				<br/>
+				
+				<hr/>
+				<p><B>Role:</b>  Project Planner, Architect</p>
+				<hr/>
+				<p><B>Skill:</b> Command over Software Planning</p>
+				<hr/>
+				<p style="font-size:22px;"><span style="font-size:42px;"></span>Software Planning is not easy task <br/>
+				
+				<span style="font-size:42px;"></span></p>
+				</div>
+			</div>
+			<div id="raj5">
+				 
+				<h1 style="box-shadow:0px 10px 50px 10px rgba(15, 94, 114, 0.56) inset;color:#ccc;text-align:center;">HARSAL CHARATE</h1>
+								 <img src="img/h.jpg" width="150px" height="200px" style="float:right;margin:2px;">
+
+				<div style="padding-top:10px; font-size:18px;width:500PX;height:300PX;"">
+				<p>helpfull guidence 
+				</p>
+				<br/>
+				
+				<hr/>
+				<p><B>Role:</b> Programmer , Designer,prof.SGSITS (software Engineering)</p>
+				<hr/>
+				<p><B>Skill:</b>  Software Engineer </p>
+				<hr/>
+				<p style="font-size:22px;"><span style="font-size:42px;"></span>Thanks sir from the Bottom of My Heart <br/>for giving me your 
+				Unlimited Support ..</p> <br> <i>By-Shailendra Tiwari</i> 
+				<span style="font-size:42px;"></span></p>
+				</div>
+			</div>
+			
+			</div>
+			
+		</div> <!-- end of wrapper class-->
+	</body>
+</html>
