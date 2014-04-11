2b72002e2ba054c4b311499d9085e7473ff42d0e
Shailendra Singh Tiwari <singhtiwari.shailendra@gmail.com>
2014-04-11 18:33:44 +0530

    Create installer.php

---
+++ installer.php
@@ -0,0 +1,40 @@
+<html>
+<head>
+<title> GSTI | Installer</title>
+		<link rel="icon" type="image/gif" href="R.gif" >
+	<style type="text/css">
+		body{font-family:Arial,'Helvetica Neue',Helvetica,sans-serif;text-align:center;background:#3c5a76 url(css/image/wallpaper-1388671.jpg) ;}
+		h1{color:#fff;font-size:45px;font-weight:bold;text-shadow:5px 5px 8px rgba(0,0,0,0.55);letter-spacing:-1px;margin:0}
+		#msg{background:rgba(38, 38, 38, .70);width:800px;height:480px; margin:80px 0 0 280px;box-shadow:4px 6px 25px 3px #fff ;border-radius:10px 10px;}
+		#msg3{padding-top:20px; }
+		p{text-shadow:0 1px 1px rgba(255,255,255,0.33);font-size:20px;line-height:25px;}
+		#msg2{border-radius:45px 45px;width:600px;padding:2px 20px 0 20px;  margin:20px 0 0 80px;border:solid 2px #ccc;background:rgba(250, 238, 238, .70);}
+		#btn ul{display:block; margin:40px 50px 0 0 ;}
+		#btn li{float:right;list-style:none; width:155px;margin-right:125px;padding-top:-2px;}
+		#btn li a:link {box-shadow:0 0 32px 0 #fff inset ;background:rgb(40, 48, 59);border-radius:20px 20px; font-size:22px;line-height:45px; padding:0 ; display: block;font-weight:normal; color: #ffffff; text-align:center; text-decoration:none; text-transform: uppercase;}
+		#btn li a:visited {box-shadow:0 0 32px 0 #fff inset ;background:rgb(40, 48, 59);border-radius:20px 20px; font-size:22px; margin-right:2px; height:35px;padding:0 ; display: block;font-weight:normal; color: #ffffff; text-align:center; text-decoration:none; text-transform: uppercase;}
+		#btn li a:hover {box-shadow:0 0 32px 0 #fff inset ; background:black;border-radius:20px 20px;   line-height:45px;padding:0 ; display: block;font-weight:normal; color:#fff; text-align:center; text-decoration:none; text-transform: uppercase; }
+
+	</style>
+</head>
+<body>
+	<div id="msg">
+	<div id="msg3">
+	<h1>Welcome to the  <i>GSTI Construction System</i>  Setup Wizard</h1>
+	<div id="msg2">
+		<p> This Setup wizard will Install and Configure GSTI Construction System system in your Pc or in 
+		your server this is recommended that you shuld Close unnecessary Application program running in your pc</p>
+		<p><b>Warning:</b>This Software is Protected by Copyright Law. All Copyright reserved @ <i>Shailendra Singh Tiwari</i><br/>
+		visit: <a href="http://singhtiwarishailendra.wix.com/shailendratiwari" target="_blank"> www.shailendra.com</a></p>
+	</div>	
+	</div>
+	<div id="btn">
+		<ul>
+		<li><a href="http://singhtiwarishailendra.wix.com/shailendratiwari" target="_blank">Cancel</a></li> 
+			<li><a href="installer2.php">Next </a></li>
+			 
+		</ul>
+		</div>
+	</div>
+</body>
+</html>
